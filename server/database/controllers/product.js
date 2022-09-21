433d58c9b344db3e935a1778ee4fb73704a716d3
Rihem BEN SAAD <66827882+rihem-bensaad@users.noreply.github.com>
2022-09-21 15:58:09 +0100

    uptodate

--- server/database/controllers/product.js
+++ server/database/controllers/product.js
@@ -1,5 +1,4 @@
 const Product = require('../models/index.js').product
-
 module.exports = {
     getProduct:   (req, res) => {
         Product.find({}, (err, result) => {
@@ -10,31 +9,27 @@ module.exports = {
         res.send(result)
     })
 },
-
- getProductById :  (req,res) => { 
-     Product.findById(req.params.id,(err,result) => {
+ getProductById :  (req, res) => { 
+     Product.findById(req.params.id,(err, result) => {
         res.send(result)
     })
 },
-
-
     addProduct: (req, res) => {
     console.log(req.body)
     const product = new Product({
        pricePromo: req.body.pricePromo,
        title: req.body.title,
-        price: req.body.price, 
+       price: req.body.price, 
        imageUrl: req.body.imageUrl,
        description: req.body.description,
-        producer: req.body.producer,
-        stock: req.body.stock,
+       producer: req.body.producer,
+       stock: req.body.stock,
        category: req.body.category
     })
      product.save(() => {
     res.send(product)
 })
 },
-
  editProduct :  (req,res) => {
      console.log(req.body)
      Product.updateOne({ '_id': req.params.id }, req.body, (err, result) => {
@@ -48,18 +43,15 @@ module.exports = {
         
     
 },
-
-
- deleteProductById :   (req,res) => {
+ deleteProductById :   (req, res) => {
      Product.deleteOne({ '_id': req.params.id }, (err, result) => {
          if (err) {
              res.status(400).send('id not found')
              return
          }
-         res.status(200).send({"message":"product deleted successfully"})
+         res.status(200).send({"message":"product deleted"})
      })
         
-    
     }
 }
 
